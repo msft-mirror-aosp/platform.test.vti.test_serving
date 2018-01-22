4f4295a50cef587450d801df029557cec0acbad3
Keun Soo Yim <yim@google.com>
2018-01-21 22:22:13 -0800

    Group build info based on dessert release version
    Test: deploy to GAE
    Bug: 72292237

--- gae/webapp/src/dashboard/build_list.py
+++ gae/webapp/src/dashboard/build_list.py
@@ -23,8 +23,11 @@ from webapp.src import webapp_config
 from webapp.src.proto import model
 
 
-def ReadBuildInfo():
+def ReadBuildInfo(target_branch=""):
     """Reads build information.
+
+    Args:
+        target_branch: string, to select a specific branch.
 
     Returns:
         a dict containing test build information,
@@ -34,19 +37,55 @@ def ReadBuildInfo():
     build_query = model.BuildModel.query()
     builds = build_query.fetch()
 
-    test_builds = []
-    device_builds = []
-    gsi_builds = []
-    for build in builds:
-        if build.artifact_type == "test":
-            test_builds.append(build)
-        elif build.artifact_type == "device":
-            device_builds.append(build)
-        elif build.artifact_type == "gsi":
-            gsi_builds.append(build)
-        else:
-            print("unknown artifact_type %s" % build.artifact_type)
+    test_builds = {}
+    device_builds = {}
+    gsi_builds = {}
 
+    if builds:
+        for build in builds:
+            if build.manifest_branch.startswith("git_oc-mr1"):
+                m_brach = "O-MR1"
+            elif build.manifest_branch.startswith("git_oc-"):
+                m_brach = "O"
+            else:
+                m_branch = "P"
+
+            if target_branch and target_branch != m_branch:
+                continue
+
+            if build.manifest_branch.startswith("git_"):
+                build.manifest_branch = build.manifest_branch.replace("git_", "")
+
+            if build.artifact_type == "test":
+                if m_branch in test_builds:
+                    test_builds[m_branch].append(build)
+                else:
+                    test_builds[m_branch] = [build]
+            elif build.artifact_type == "device":
+                if m_branch in device_builds:
+                   device_builds[m_branch].append(build)
+                else:
+                   device_builds[m_branch] = [build]
+            elif build.artifact_type == "gsi":
+                if m_branch in gsi_builds:
+                   gsi_builds[m_branch].append(build)
+                else:
+                   gsi_builds[m_branch] = [build]
+            else:
+                print("unknown artifact_type %s" % build.artifact_type)
+
+    if test_builds:
+        for m_branch in test_builds:
+            test_builds[m_branch] = sorted(
+                test_builds[m_branch], key=lambda x: x.build_id, reverse=True)
+    if device_builds:
+        for m_branch in device_builds:
+            device_builds[m_branch] = sorted(
+                device_builds[m_branch], key=lambda x: x.build_id, reverse=True)
+    if gsi_builds:
+        for m_branch in gsi_builds:
+            gsi_builds[m_branch] = sorted(
+                gsi_builds[m_branch], key=lambda x: x.build_id, reverse=True)
     return test_builds, device_builds, gsi_builds
 
 
@@ -55,7 +94,9 @@ class BuildPage(webapp2.RequestHandler):
 
     def get(self):
         """Generates an HTML page based on the build info kept in DB."""
-        test_builds, device_builds, gsi_builds = ReadBuildInfo()
+        target_branch = self.request.get("branch", default_value="")
+
+        test_builds, device_builds, gsi_builds = ReadBuildInfo(target_branch)
         user = users.get_current_user()
         if user:
             url = users.create_logout_url(self.request.uri)
@@ -64,11 +105,31 @@ class BuildPage(webapp2.RequestHandler):
             url = users.create_login_url(self.request.uri)
             url_linktext = "Login"
 
+        manifest_branch_keys =  list(set().union(
+            test_builds.keys(), device_builds.keys(),
+            gsi_builds.keys()))
+        all_builds = {}
+        for manifest_branch_key in manifest_branch_keys:
+            all_builds[manifest_branch_key] = {}
+            if manifest_branch_key in test_builds:
+                all_builds[manifest_branch_key]["test"] = test_builds[
+                    manifest_branch_key]
+            else:
+                all_builds[manifest_branch_key]["test"] = []
+            if manifest_branch_key in device_builds:
+                all_builds[manifest_branch_key]["device"] = device_builds[
+                    manifest_branch_key]
+            else:
+                all_builds[manifest_branch_key]["device"] = []
+            if manifest_branch_key in gsi_builds:
+                all_builds[manifest_branch_key]["gsi"] = gsi_builds[
+                    manifest_branch_key]
+            else:
+                all_builds[manifest_branch_key]["gsi"] = []
+
         template_values = {
             "user": user,
-            "test_builds": test_builds,
-            "device_builds": device_builds,
-            "gsi_builds": gsi_builds,
+            "all_builds": all_builds,
             "url": url,
             "url_linktext": url_linktext,
         }
