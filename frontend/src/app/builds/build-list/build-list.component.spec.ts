b9fccafaf910b5bbda62365c13075cd6e83e6b36
Jongmok Hong <jongmok@google.com>
2018-03-15 01:09:50 +0000

    Revert "Cleanup test cases"
    This reverts commit 5ab87b69cd8be946147ad9c54e3b4dcba36dcc4f.
    
    Reason for revert: Removing all angular frontend codes
    Bug: 74575555
    Change-Id: Id1fc931ac7bbe4038b1e4c3f3a74c0213bdfae9c

--- frontend/src/app/builds/build-list/build-list.component.spec.ts
+++ frontend/src/app/builds/build-list/build-list.component.spec.ts
@@ -1,12 +1,12 @@
 import {async, TestBed} from '@angular/core/testing';
 import {APP_BASE_HREF} from '@angular/common';
-import {BuildListComponent} from './build-list.component';
-import {BuildModule} from '../build.module';
+import {JobListComponent} from './job-list.component';
+import {JobsModule} from '../jobs.module';
 import {TestsModule} from '../../shared/modules/tests.module';
 import {TranslateModule} from '@ngx-translate/core';
 import {APP_CONFIG, AppConfig} from '../../config/app.config';
 
-describe('BuildListComponent', () => {
+describe('HeroListComponent', () => {
   let fixture;
   let component;
 
@@ -15,7 +15,7 @@ describe('BuildListComponent', () => {
       imports: [
         TestsModule,
         TranslateModule.forRoot(),
-        BuildModule
+        JobsModule
       ],
       providers: [
         {provide: APP_CONFIG, useValue: AppConfig},
@@ -23,12 +23,12 @@ describe('BuildListComponent', () => {
       ],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(BuildListComponent);
+    fixture = TestBed.createComponent(JobListComponent);
     fixture.detectChanges();
     component = fixture.debugElement.componentInstance;
   }));
 
-  it('should create build list component', (() => {
+  it('should create hero list component', (() => {
     expect(component).toBeTruthy();
   }));
 });
